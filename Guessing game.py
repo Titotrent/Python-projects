793a8a432f970aed2615072a515fe039c1c0ecaf
Titotrent <110603668+Titotrent@users.noreply.github.com>
2023-05-30 04:18:24 -0700

    Guessing game
     A good application  of  while loops

---
+++ Guessing game.py
@@ -0,0 +1,14 @@
+# The code below allows the user to guess the programmers favourite color 3 times
+favorite_color= "purple"
+guess_count=1
+guess_limit=3
+while guess_count<=guess_limit:
+    guess=input('Guess my favorite color: ')
+    print('Wrong guess')
+    guess_count+=1 # this always come before the if statements
+    if guess==favorite_color:
+        print('Correct guess')
+        break
+else:
+    print('You lost ')
+    print('My favorite color is purple red')
